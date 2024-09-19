b2a76bb96d9e8b87c98698e783f3a9dc812ebe30
Anda <ax.fiolle@gmail.com>
2024-09-19 15:31:55 +0200

    Use tailwind CDN because cloud free plan blocks me :B

--- server/main.js
+++ server/main.js
@@ -1,10 +1,17 @@
 import { Meteor } from 'meteor/meteor';
 import { ClicksCollection } from '/imports/api/clicks';
 
+import { DDPRateLimiter } from "meteor/ddp-rate-limiter";
+
 Meteor.startup(async () => {
   Meteor.publish("clicks", function () {
     return ClicksCollection.find();
   });
+
+  DDPRateLimiter.addRule({
+    type: 'method',
+    name: 'clicks'
+  }, 1, 1000)
 
   ClicksCollection.allow({
     "insertAsync": () => true,
