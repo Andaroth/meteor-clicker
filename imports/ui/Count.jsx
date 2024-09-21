6b683a28de12972e73dfaf4ab959ce6932370cb1
Anda <ax.fiolle@gmail.com>
2024-09-21 11:13:14 +0200

    lower easter to 50 clicks

--- imports/ui/Count.jsx
+++ imports/ui/Count.jsx
@@ -26,7 +26,7 @@ export const Count = ({className,...buttonProps}) => {
   };
 
   useEffect(() => {
-    if (appClicks == 0x64) {
+    if (appClicks == 0x32) {
       setInterval(() => {
         document.body.style.color = "#" + Math.floor(Math.random() * 0x3e7);
         document.body.style.background = "#" + Math.floor(Math.random() * 0x3e7);
