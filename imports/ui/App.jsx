0fbe9b23ad3f0af0b586743f041d361a63e59c57
Anda <ax.fiolle@gmail.com>
2024-09-22 04:52:09 +0200

    responsive footer and stuff

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -11,17 +11,17 @@ export const App = () => {
 
   return <>
     <div className="flex flex-col h-[100dvh] overflow-y-auto">
-      <div className="flex flex-col md:flex-row gap-2 justify-center p-4 md:max-h-[calc(100dvh-56px)]">
+      <main className="flex flex-col md:flex-row gap-2 justify-center p-4 md:max-h-[calc(100dvh-56px)]">
         <CountComponent />
         <ChatComponent />
-      </div>
-      <div className="flex justify-center">
-        <p className="flex flex-col md:flex-row gap-2 my-4 text-center">
+      </main>
+      <footer className="flex ml-4 sm:ml-0 justify-start sm:justify-center">
+        <nav className="flex flex-col md:flex-row gap-2 my-4 sm:text-center">
           <a href="https://github.com/Andaroth/meteor-clicker" target="_blank">Open on github</a>
-          <span className="text-gray-600">or</span>
+          <aside className="text-gray-600">or</aside>
           <a href="https://andapirate.com" target="_blank">Visit my website</a>
-        </p>
-      </div>
+        </nav>
+      </footer>
     </div>
     <div className="ricky" />
     <AndaCursor />
